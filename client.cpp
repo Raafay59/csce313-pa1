0206ff63202c9e1304e0cff69d9c3140dce34f3b
Raafay59 <raafay.ah@gmail.com>
2025-09-28 19:07:49 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -6,14 +6,23 @@
     Date: 2/8/20
 	
 	Please include your Name, UIN, and the date below
-	Name:
-	UIN:
-	Date:
+	Name: Raafay Hemani
+	UIN: 934000608
+	Date: 9-28-2025
 */
 #include "common.h"
 #include "FIFORequestChannel.h"
 
 using namespace std;
+
+/*
+PART 1:
+Run the server process as a child of the client process using fork() and exec() such that you do not need two terminals.
+The outcome is that you open a single terminal, run the client which first runs the server and then connects to it.
+To make sure that the server does not keep running after the client dies, send a QUIT_MSG to the server for each open channel and call the wait(...) function to wait for its end.
+Thoughts:
+fork from client and start server using exec
+*/
 
 
 int main (int argc, char *argv[]) {
@@ -38,6 +47,19 @@ int main (int argc, char *argv[]) {
 				filename = optarg;
 				break;
 		}
+	}
+
+	// start server as child process (part 1)
+	pid_t pid = fork();
+    if (pid == -1) {
+		cerr << "Fork failed!" << endl;
+		return 1;
+	} else if (pid == 0) {
+		// exec server
+		// server main declaration: int main (int argc, char *argv[])
+		execvp("./server", NULL);
+		cerr << "Exec failed!" << endl;
+		return 1;
 	}
 
     FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
@@ -65,6 +87,11 @@ int main (int argc, char *argv[]) {
 	delete[] buf2;
 	
 	// closing the channel    
+	// send a QUIT_MSG to the server and wait for the server to exit (part 1)
     MESSAGE_TYPE m = QUIT_MSG;
     chan.cwrite(&m, sizeof(MESSAGE_TYPE));
+	int status;
+	waitpid(pid, &status, 0);
+	cout << "Client exiting" << endl;
+	return 0;
 }
